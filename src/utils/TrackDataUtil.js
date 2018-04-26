30728c87c3838dc3ed1917b43daac5a038c749dd
Flynn Platt <flynn.platt@jpl.nasa.gov>
2018-04-26 15:53:59 -0700

    track data util re-structure

--- src/utils/TrackDataUtil.js
+++ src/utils/TrackDataUtil.js
@@ -2,41 +2,44 @@ import appConfig from "constants/appConfig";
 
 export default class TrackDataUtil {
     static getUrlsForQuery(options) {
+        return TrackDataUtil.getDecimationQuery(options);
+        // return TrackDataUtil.getSolrQuery(options);
+    }
+
+    static getDecimationQuery(options) {
         let { selectedTracks, xAxis, yAxis, zAxis } = options;
-
-        // let baseUrl = appConfig.URLS.solrBase;
         let baseUrl = appConfig.URLS.decimatorMiddleware;
+        return selectedTracks.map(track => {
+            return encodeURI(
+                baseUrl +
+                    "?" +
+                    ["project=" + track.project, "source_id=" + track.source_id].join("&")
+            );
+        });
+    }
 
-        // let query = [
-        //     "facet=on",
-        //     "wt=csv",
-        //     "rows=3000000",
-        //     "sort=measurement_date_time asc",
-        //     "fl=measurement_date_time,depth,measurement_value"
-        // ];
-        // return selectedTracks.map(track => {
-        //     return encodeURI(
-        //         baseUrl +
-        //             "?" +
-        //             query
-        //                 .concat([
-        //                     "q=project:" +
-        //                         track.project +
-        //                         " AND source_id:" +
-        //                         track.source_id +
-        //                         " AND measurement_name:temperature"
-        //                 ])
-        //                 .join("&")
-        //     );
-        // });
-
-        let query = [];
+    static getSolrQuery(options) {
+        let { selectedTracks, xAxis, yAxis, zAxis } = options;
+        let baseUrl = appConfig.URLS.solrBase;
+        let query = [
+            "facet=on",
+            "wt=csv",
+            "rows=3000000",
+            "sort=measurement_date_time asc",
+            "fl=measurement_date_time,depth,measurement_value"
+        ];
         return selectedTracks.map(track => {
             return encodeURI(
                 baseUrl +
                     "?" +
                     query
-                        .concat(["project=" + track.project, "source_id=" + track.source_id])
+                        .concat([
+                            "q=project:" +
+                                track.project +
+                                " AND source_id:" +
+                                track.source_id +
+                                " AND measurement_name:temperature"
+                        ])
                         .join("&")
             );
         });
