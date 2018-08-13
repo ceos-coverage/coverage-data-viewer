e04e5af65429591ff0beac4a870d44597a0ed420
Flynn Platt <flynn.platt@jpl.nasa.gov>
2018-08-13 11:32:23 -0700

    moving url config to top level for ease of deployment

--- src/config.js
+++ src/config.js
@@ -39,4 +39,11 @@ APPLICATION_CONFIG = {
 
 */
 
-APPLICATION_CONFIG = {};
+APPLICATION_CONFIG = {
+    URLS: {
+        decimatorMiddleware: "https://oiip.jpl.nasa.gov/getDecData"
+        // decimatorMiddleware: "http://icewhale.jpl.nasa.gov:8100/getData"
+        // decimatorMiddleware: "http://babywhale1.jpl.nasa.gov:8100/getData/"
+        // decimatorMiddleware: "http://localhost:8100/getData"
+    }
+};
