2fffc676bf25dbb8481ca159e9e9632acecef748
Flynn Platt <flynn.platt@jpl.nasa.gov>
2021-10-15 14:01:01 -0700

    add bubble legend

--- src/components/LayerMenu/InsituLayerItem.js
+++ src/components/LayerMenu/InsituLayerItem.js
@@ -9,17 +9,18 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import RemoveIcon from "@material-ui/icons/Close";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
 import Typography from "@material-ui/core/Typography";
 import { IconButtonSmall, EnhancedTooltip, LoadingSpinner } from "_core/components/Reusables";
 import { SingleColorSelector } from "components/Reusables";
 import { InsituLayerItemTools } from "components/LayerMenu";
+import * as appStrings from "constants/appStrings";
 import * as appActions from "actions/appActions";
 import * as mapActions from "actions/mapActions";
 import * as mapActionsCore from "_core/actions/mapActions";
 import styles from "components/LayerMenu/InsituLayerItem.scss";
+import textStyles from "_core/styles/text.scss";
 import MiscUtil from "utils/MiscUtil";
 
 export class InsituLayerItem extends Component {
@@ -51,7 +52,11 @@ export class InsituLayerItem extends Component {
             this.props.layer.get("title").size > 0
                 ? this.props.layer.getIn(["title", 0])
                 : this.props.layer.get("title");
+
         subtitle = subtitle.size > 0 ? subtitle.get(0) : subtitle;
+
+        const bubbleLegend =
+            this.props.layer.get("handleAs") === appStrings.LAYER_VECTOR_POINTS_WFS;
 
         const visible = this.props.layer.get("opacity") === 1;
         const visIcon = visible ? <VisibilityIcon /> : <VisibilityOffIcon />;
@@ -61,48 +66,107 @@ export class InsituLayerItem extends Component {
             [styles.visibilityOff]: !visible
         });
 
+        const color = this.props.layer.get("vectorColor");
+        const units = this.props.layer.getIn(["insituMeta", "variables_units", 0]);
+
         return (
             <div key={this.props.layer.get("id") + "-insitu-menu-item"} className={rootClasses}>
-                <div className={styles.leftItem}>{this.renderLeftAction()}</div>
-                <div className={styles.centerItem}>
-                    <EnhancedTooltip
-                        disableFocusListener={true}
-                        title={subtitle}
-                        placement="bottom"
-                        PopperProps={{
-                            modifiers: {
-                                preventOverflow: {
-                                    enabled: false
-                                },
-                                hide: {
-                                    enabled: false
+                <div className={styles.main}>
+                    <div className={styles.leftItem}>{this.renderLeftAction()}</div>
+                    <div className={styles.centerItem}>
+                        <EnhancedTooltip
+                            disableFocusListener={true}
+                            title={subtitle}
+                            placement="bottom"
+                            PopperProps={{
+                                modifiers: {
+                                    preventOverflow: {
+                                        enabled: false
+                                    },
+                                    hide: {
+                                        enabled: false
+                                    }
                                 }
-                            }
-                        }}
-                    >
-                        <Typography
-                            variant="body2"
-                            color="inherit"
-                            component="span"
-                            className={styles.label}
+                            }}
                         >
-                            {`${title} (id: ${this.props.layer.get("shortId")})`}
-                        </Typography>
-                    </EnhancedTooltip>
+                            <Typography
+                                variant="body2"
+                                color="inherit"
+                                component="span"
+                                className={styles.label}
+                            >
+                                {`${title} (id: ${this.props.layer.get("shortId")})`}
+                            </Typography>
+                        </EnhancedTooltip>
+                    </div>
+                    <div className={styles.rightItem}>
+                        <InsituLayerItemTools layer={this.props.layer} />
+                        <IconButtonSmall
+                            color="inherit"
+                            className={styles.actionBtn}
+                            disabled={this.props.layer.get("isLoading")}
+                            onClick={() =>
+                                this.props.setLayerOpacity(this.props.layer, visible ? 0 : 1)
+                            }
+                        >
+                            {visIcon}
+                        </IconButtonSmall>
+                    </div>
                 </div>
-                <div className={styles.rightItem}>
-                    <InsituLayerItemTools layer={this.props.layer} />
-                    <IconButtonSmall
-                        color="inherit"
-                        className={styles.actionBtn}
-                        disabled={this.props.layer.get("isLoading")}
-                        onClick={() =>
-                            this.props.setLayerOpacity(this.props.layer, visible ? 0 : 1)
-                        }
-                    >
-                        {visIcon}
-                    </IconButtonSmall>
-                </div>
+                {bubbleLegend ? (
+                    <div className={styles.bubbleLegend}>
+                        <div className={styles.bubbleWrapper}>
+                            <div
+                                style={{ background: color }}
+                                className={`${styles.bubble} ${styles.bubbleSize1}`}
+                            />
+                            <div className={`${styles.value} ${textStyles.fontRobotoMono}`}>0</div>
+                        </div>
+                        <div className={styles.bubbleWrapper}>
+                            <div
+                                style={{ background: color }}
+                                className={`${styles.bubble} ${styles.bubbleSize2}`}
+                            />
+                            <div className={`${styles.value} ${textStyles.fontRobotoMono}`}>1</div>
+                        </div>
+                        <div className={styles.bubbleWrapper}>
+                            <div
+                                style={{ background: color }}
+                                className={`${styles.bubble} ${styles.bubbleSize3}`}
+                            />
+                            <div className={`${styles.value} ${textStyles.fontRobotoMono}`}>
+                                3.5
+                            </div>
+                        </div>
+                        <div className={styles.bubbleWrapper}>
+                            <div
+                                style={{ background: color }}
+                                className={`${styles.bubble} ${styles.bubbleSize4}`}
+                            />
+                            <div className={`${styles.value} ${textStyles.fontRobotoMono}`}>10</div>
+                        </div>
+                        <div className={styles.bubbleWrapper}>
+                            <div
+                                style={{ background: color }}
+                                className={`${styles.bubble} ${styles.bubbleSize5}`}
+                            />
+                            <div className={`${styles.value} ${textStyles.fontRobotoMono}`}>20</div>
+                        </div>
+                        <div className={styles.bubbleWrapper}>
+                            <div
+                                style={{ background: color }}
+                                className={`${styles.bubble} ${styles.bubbleSize6}`}
+                            />
+                            <div className={`${styles.value} ${textStyles.fontRobotoMono}`}>50</div>
+                        </div>
+                        <div className={styles.bubbleWrapper}>
+                            <div className={`${styles.bubble} ${styles.invisibleBubble}`} />
+                            <div className={`${styles.value} ${textStyles.fontRobotoMono}`}>
+                                {units}
+                            </div>
+                        </div>
+                    </div>
+                ) : null}
             </div>
         );
     }
